8508860015e0f25782d44e4e5644c3a31218b06b
Guigolinus <guigolinus@users.noreply.github.com>
2026-08-12 21:37:53 +0000

    feat(runtime): add polkavm feature flag and update build.rs docs

--- runtime/build.rs
+++ runtime/build.rs
@@ -5,6 +5,9 @@ fn main() {
 			.with_current_project()
 			.export_heap_base()
 			.import_memory()
+			// TODO (Fase PolkaVM): Adicionar .build_polkavm() quando migrar para
+			// polkadot-sdk >= stable2409 que suporta PolkaVM como target alternativo.
+			// Ref: https://github.com/paritytech/polkadot-sdk/pull/3741
 			.build();
 	}
 }
